586a53a558c48220fea24d70f0cbb258e4fc7c99
Raffaz7X <88053609+Raffaz7X@users.noreply.github.com>
2022-01-04 22:41:44 -0300

    visto

---
+++ experiencein/perfis/migrations/0003_perfil_contatos.py
@@ -0,0 +1,17 @@
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('perfis', '0002_convite'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='perfil',
+            name='contatos',
+            field=models.ManyToManyField(related_name='_perfil_contatos_+', to='perfis.Perfil'),
+        ),
+    ]
